34defb824d16c8827966ca670f5471555ff2586e
Tung Teng <thetungctn@gmail.com>
2025-12-16 13:06:03 +0700

    Setup fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,20 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import { Inter, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  fallback: ["sans-serif"],
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+  fallback: ["monospace"],
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${geistMono.variable}`}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
