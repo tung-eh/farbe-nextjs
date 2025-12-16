1c6f9d968676bf3191412801995e82a3af09bdde
Tung Teng <thetungctn@gmail.com>
2025-12-16 17:10:37 +0700

    Brand signature

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 import type { SettingsDocument } from "@root/prismicio-types";
+
+import BrandSignature from "./BrandSignature";
 
 const Header = ({ settings }: { settings: SettingsDocument }) => {
   const links = settings.data.navigation;
 
   return (
-    <header className="flex justify-between">
-      logo
-      <div className="max-w-[50vw]">{JSON.stringify(links)}</div>
+    <header>
+      <nav className="flex justify-between">
+        <Link href="/">
+          <BrandSignature className="h-[1em] w-auto" />
+        </Link>
+        <ul className="max-w-[50vw]">{JSON.stringify(links)}</ul>
+      </nav>
     </header>
   );
 };
