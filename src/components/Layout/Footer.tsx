dc8eb7c26e5f85d2e1465ddd79b2bb320a3b4c78
Tung Teng <thetungctn@gmail.com>
2025-12-17 09:16:39 +0700

    Render screen height logo in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,16 @@
 import type { SettingsDocument } from "@root/prismicio-types";
 
+import Logo from "./Logo";
+
 const Footer = ({ settings }: { settings: SettingsDocument }) => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="relative z-20 bg-black text-white">
+      <figure className="absolute inset-0">
+        <Logo className="h-screen w-full xl:w-auto" />
+      </figure>
+      <section className="min-h-screen"></section>
+    </footer>
+  );
 };
 
 export default Footer;
