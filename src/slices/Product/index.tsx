ea9d11f90fe2e54a2f3a6141aed37ded2b1ccfe0
Tung Teng <thetungctn@gmail.com>
2025-12-18 16:30:34 +0700

    Get product data from slice

--- src/slices/Product/index.tsx
+++ src/slices/Product/index.tsx
@@ -1,28 +1,28 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { isFilled, Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
-/**
- * Props for `Product`.
- */
 export type ProductProps = SliceComponentProps<Content.ProductSlice>;
 
-/**
- * Component for "Product" Slices.
- */
+const getProduct = (slice: ProductProps["slice"]) => {
+  const { product } = slice.primary;
+
+  if (!isFilled.contentRelationship(product)) {
+    return undefined;
+  }
+
+  return product;
+};
+
 const Product: FC<ProductProps> = ({ slice }) => {
+  const product = getProduct(slice);
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for product (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <PrismicRichText field={product?.data?.name} />
     </section>
   );
 };
