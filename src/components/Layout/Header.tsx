ba42b9432d86fec9de89a348012d0dbe43413b74
Tung Teng <thetungctn@gmail.com>
2025-12-18 14:44:59 +0700

    Backdrop blur the whole navbar in mobile
    Instead only the nav links (like in desktop)

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,11 +10,11 @@ const Header = ({ settings }: { settings: SettingsDocument }) => {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-10">
-      <nav className="flex justify-between">
+      <nav className="flex justify-between backdrop-blur xl:backdrop-none">
         <Link href="/" className="cta ml-4">
           <BrandSignature className="text-lg" />
         </Link>
-        <ul className="xl:w-[calc(40%+2rem)] flex items-center pl-8 pr-4 backdrop-blur">
+        <ul className="xl:w-[calc(40%+2rem)] flex items-center pl-8 pr-4 xl:backdrop-blur">
           {links.map((link) => (
             <li key={link.key} className="hidden xl:block">
               <PrismicNextLink field={link} className="cta" />
