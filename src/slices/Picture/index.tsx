e68692135bc05807650b7253f947d4e97d4bcf1b
Tung Teng <thetungctn@gmail.com>
2025-12-18 13:57:59 +0700

    Render Picture slice

--- src/slices/Picture/index.tsx
+++ src/slices/Picture/index.tsx
@@ -1,28 +1,31 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { isFilled, type Content } from "@prismicio/client";
+import { PrismicRichText, type SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
 
-/**
- * Props for `Picture`.
- */
 export type PictureProps = SliceComponentProps<Content.PictureSlice>;
 
-/**
- * Component for "Picture" Slices.
- */
 const Picture: FC<PictureProps> = ({ slice }) => {
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="grid xl:grid-cols-[3fr_2fr]"
     >
-      Placeholder component for picture (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <figure>
+        <PrismicNextImage field={slice.primary.picture} />
+        <figcaption>
+          <PrismicRichText field={slice.primary.caption} />
+        </figcaption>
+      </figure>
+      {isFilled.image(slice.primary.secondary_picture) && (
+        <figure>
+          <PrismicNextImage field={slice.primary.secondary_picture} />
+          <figcaption>
+            <PrismicRichText field={slice.primary.secondary_caption} />
+          </figcaption>
+        </figure>
+      )}
     </section>
   );
 };
