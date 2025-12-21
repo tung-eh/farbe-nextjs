12095091da3342143aeebee14bf41218eb6176d8
Tung Teng <thetungctn@gmail.com>
2025-12-22 03:34:23 +0700

    Add random mesh & light to test if Canvas setup correctly

--- src/components/Layout/Canvas/index.tsx
+++ src/components/Layout/Canvas/index.tsx
@@ -5,7 +5,15 @@ import { Canvas as R3fCanvas } from "@react-three/fiber";
 const Canvas = ({ className }: { className?: string }) => {
   return (
     <figure className={className}>
-      <R3fCanvas />
+      <R3fCanvas>
+        <mesh>
+          <torusGeometry args={[1, 0.4, 16, 100]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} />
+      </R3fCanvas>
     </figure>
   );
 };
