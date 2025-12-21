dda5876868528624278b065dbb6192563998a5e1
Tung Teng <thetungctn@gmail.com>
2025-12-22 03:35:45 +0700

    Add perspective camera

--- src/components/Layout/Canvas/index.tsx
+++ src/components/Layout/Canvas/index.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { Canvas as R3fCanvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
 
 const Canvas = ({ className }: { className?: string }) => {
   return (
     <figure className={className}>
       <R3fCanvas>
+        <PerspectiveCamera
+          makeDefault
+          fov={45}
+          position={[0, 0, 20]}
+          lookAt={[0, 0, 0]}
+        />
+
         <mesh>
           <torusGeometry args={[1, 0.4, 16, 100]} />
           <meshStandardMaterial color="orange" />
