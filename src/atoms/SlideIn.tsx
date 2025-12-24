1203057c88a5258017965a9efddc51b0fd7dd4a9
Tung Teng <thetungctn@gmail.com>
2025-12-24 19:01:33 +0700

    Fix first render SlideIn component

--- src/atoms/SlideIn.tsx
+++ src/atoms/SlideIn.tsx
@@ -4,17 +4,20 @@ import { useRef, createElement, JSX } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import { twMerge } from "tailwind-merge";
 
 gsap.registerPlugin(ScrollTrigger);
 
 type SlideInProps<T extends keyof JSX.IntrinsicElements> = {
   as?: T;
   scrollTrigger?: ScrollTrigger.Vars;
+  className?: string;
 } & Omit<JSX.IntrinsicElements[T], "ref">;
 
 const SlideIn = <T extends keyof JSX.IntrinsicElements = "section">({
   as,
   scrollTrigger,
+  className,
   ...props
 }: SlideInProps<T>) => {
   const ref = useRef<HTMLElement>(null);
@@ -22,9 +25,9 @@ const SlideIn = <T extends keyof JSX.IntrinsicElements = "section">({
   useGSAP(() => {
     if (!ref.current) return;
 
-    gsap.from(ref.current.children, {
-      opacity: 0,
-      y: 50,
+    gsap.to(ref.current.children, {
+      opacity: 1,
+      y: 0,
       delay: 0.3,
       duration: 1,
       ease: "power2.out",
@@ -40,6 +43,7 @@ const SlideIn = <T extends keyof JSX.IntrinsicElements = "section">({
   // eslint-disable-next-line react-hooks/refs
   return createElement(as ?? "section", {
     ref,
+    className: twMerge("[&>*]:opacity-0 [&>*]:translate-y-[50px]", className),
     ...props,
   });
 };
